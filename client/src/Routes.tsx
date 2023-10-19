f0fa9230fec77bdaf049d8afd8b7b69a35bf691a
Abraham Alemayehu <85797846+aaabrsh@users.noreply.github.com>
2023-10-19 12:33:28 +0000

    added signup and forgot password pages

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,12 +1,20 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Signup from "./pages/auth/Signup";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+
+        <Route path="auth/">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+        </Route>
 
         {/* <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
