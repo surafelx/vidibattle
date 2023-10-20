974e41c3a858198557e67521c9d6d0fa9d5eef75
abraham <aaabrham421@gmail.com>
2023-10-20 12:53:07 +0300

    added mongodb connection and minor changes

--- server/app.js
+++ server/app.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const routes = require('./routes/api');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+require("dotenv").config({ path: "./config.env" });
+
+const cors = require("cors");
+
+const connect = mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000; app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
-app.use('/api', routes);
-
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
 app.use((err, req, res, next) => {
-    console.log(err);
-    next();
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => {
