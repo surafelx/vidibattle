846edacc38e2cd22aaaf353d7b02cddc64823696
abraham <aaabrham421@gmail.com>
2023-10-23 11:47:21 +0300

    added homepage and configured routing with auth guard

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -1,29 +1,55 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import SplashScreen from "./components/SplashScreen";
 import Login from "./pages/auth/Login";
 import WhatsAppLogin from "./pages/auth/WhatsAppLogin";
 import Welcome from "./pages/auth/Welcome";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/NotFound";
 
 export default function Router() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // TODO: check if user is logged in
+    setLoading(false);
+    setLoggedIn(true);
+  });
+
+  if (loading) {
+    return <SplashScreen />;
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-
-        <Route path="auth/">
-          <Route index element={<Welcome />} />
-          <Route path="login/">
-            <Route index element={<Login />} />
-            <Route path="whatsapp/" element={<WhatsAppLogin />} />
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="auth/">
+            <Route index element={<Welcome />} />
+            <Route path="login/">
+              <Route index element={<Login />} />
+              <Route path="whatsapp/" element={<WhatsAppLogin />} />
+            </Route>
           </Route>
-        </Route>
+          {loggedIn ? (
+            <>
+              <Route path="/" element={<Navigate to="/home" />}></Route>
+              <Route path="/home" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
 
-        {/* <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route> */}
-      </Routes>
-    </BrowserRouter>
+              {/* <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="blogs" element={<Blogs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<NoPage />} />
+                  </Route> */}
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/auth" replace />} />
+          )}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
