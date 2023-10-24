4818300a0666f251bd31940ec685dcae38215f4a
abraham <aaabrham421@gmail.com>
2023-10-24 14:19:22 +0300

    refactor: add http-error package, refactor code on app.js page

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
+const createError = require("http-errors");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const routes = require("./routes/api");
@@ -15,57 +16,69 @@ const connect = mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+  .then(() => {
+    console.log("MongoDB Connected...");
 
-const port = process.env.PORT || 5000;
+    const app = express();
 
-const app = express();
+    // middlewares
+    app.use(
+      session({
+        secret: process.env.SESSION_SECRET || "twinphy",
+        resave: false,
+        store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI ?? "" }),
+        saveUninitialized: true,
+        cookie: {
+          maxAge: 1000 * 60 * 60 * 24, // 1 day
+        },
+      })
+    );
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(
+      cors()
+      // TODO: configure cors
+      // cors({
+      //   origin: process.env.CLIENT_URL,
+      //   methods: "GET,POST,PUT,DELETE",
+      //   credentials: true,
+      // })
+    );
+    app.use(express.json());
+    app.use("/api", routes);
 
-// middlewares
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || "twinphy",
-    resave: false,
-    store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI ?? "" }),
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404, "Page Not Found"));
+    });
+
+    // error handler middleware
+    app.use((err, req, res, next) => {
+      const statusCode = err.statusCode || 500;
+      console.error(err.statusCode, err.message, err.stack);
+      res.status(statusCode).json({ success: false, message: err.message });
+
+      return;
+    });
+
+    const port = process.env.PORT || 5000;
+
+    // https
+    //   .createServer(
+    //     // Provide the private and public key to the server by reading each
+    //     // file's content with the readFileSync() method.
+    //     {
+    //       key: fs.readFileSync("localhost-key.pem"),
+    //       cert: fs.readFileSync("localhost.pem"),
+    //     },
+    //     app
+    //   )
+    //   .listen(port, () => {
+    //     console.log(`Server running on port ${port}`);
+    //   });
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(
-  cors()
-  // TODO: configure cors
-  // cors({
-  //   origin: process.env.CLIENT_URL,
-  //   methods: "GET,POST,PUT,DELETE",
-  //   credentials: true,
-  // })
-);
-app.use(express.json());
-app.use("/api", routes);
-app.use((err, req, res, next) => {
-  console.log(err);
-  next();
-});
-
-// https
-//   .createServer(
-//     // Provide the private and public key to the server by reading each
-//     // file's content with the readFileSync() method.
-//     {
-//       key: fs.readFileSync("localhost-key.pem"),
-//       cert: fs.readFileSync("localhost.pem"),
-//     },
-//     app
-//   )
-//   .listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-//   });
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  .catch((err) => console.log("MongoDB Connection Error:", err));
