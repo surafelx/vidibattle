85369c40debd9ae2d577a731cf266eb923589c72
abraham <aaabrham421@gmail.com>
2023-10-26 18:02:17 +0300

    feat: create routes from chat

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth.route");
-const postRouter = require("./posts.route");
+const postRouter = require("./post.route");
 const mediaRouter = require("./media.route");
+const chatRouter = require("./chat.route");
 
 router.use("/auth", authRouter);
 router.use("/post", postRouter);
 router.use("/media", mediaRouter);
+router.use("/chat", chatRouter);
 
 module.exports = router;
