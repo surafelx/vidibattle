a1110809c55e65a472ce338618520a9b570bc21b
abraham <aaabrham421@gmail.com>
2023-10-26 19:37:18 +0300

    integrate chat list with api

--- client/src/pages/chat/ChatList.tsx
+++ client/src/pages/chat/ChatList.tsx
@@ -1,18 +1,44 @@
 import ChatListHeader from "./components/ChatListHeader";
 import SearchBar from "../../components/SearchBar";
 import ContactsList from "./components/container/ContactsList";
+import { useEffect, useState } from "react";
+import PageLoading from "../../components/PageLoading";
+import { get } from "../../services/crud";
 
 export default function ChatList() {
+  const [pageLoading, setPageLoading] = useState(true);
+  const [chatsList, setChatsList] = useState([]);
+  const [chatsCount, setChatsCount] = useState(0);
+
+  useEffect(() => {
+    // TODO: change id
+    get("chat/list/" + "653a5e9300ecfb67556b51aa")
+      .then((res) => {
+        console.log(res);
+        setChatsCount(res.count);
+        setChatsList(res.data);
+        setPageLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setPageLoading(false);
+      });
+  }, []);
+
+  if (pageLoading) {
+    return <PageLoading />;
+  }
+
   return (
     <>
       <ChatListHeader />
 
-      <div className="page-content">
+      <div className="page-content vh-100">
         <div className="content-inner pt-0">
           <div className="container bottom-content">
             <SearchBar />
 
-            <ContactsList />
+            <ContactsList list={chatsList} />
           </div>
         </div>
       </div>
--- client/src/pages/chat/components/container/ContactsList.tsx
+++ client/src/pages/chat/components/container/ContactsList.tsx
@@ -1,182 +1,62 @@
 import { Link } from "react-router-dom";
+import timeAgo from "../../../../services/timeAgo";
 
-export default function ContactsList() {
+export default function ContactsList({ list }: { list: any[] }) {
+  // TODO: change current id with loggedin user id
+  const currentUserId = "653a5e9300ecfb67556b51aa";
+
   return (
     <>
       <ul className="dz-list message-list">
         <li>
-          <Link to="1">
-            <div className="media media-50">
-              <img
-                className="rounded"
-                src="/assets/images/message/pic1.jpg"
-                alt="image"
-              />
-            </div>
-            <div className="media-content">
-              <div>
-                <h6 className="name">Lucas Mokmana</h6>
-                <p className="my-1">
-                  Hey bro, let's meetup at centre point corner
-                </p>
-              </div>
-              <div className="left-content">
-                <span className="time">2m ago</span>
-                <div className="seen-btn active mt-2">
-                  <svg
-                    width="11"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
-                      fill="#BBB6D0"
-                    />
-                  </svg>
+          {list.map((li: any) => {
+            const secondUser =
+              li.participants?.[0]?._id === currentUserId
+                ? li.participants?.[1]
+                : li.participants?.[0];
+            return (
+              <Link to={li._id}>
+                <div className="media media-50">
+                  <img
+                    className="rounded"
+                    src={secondUser?.profile_img}
+                    alt="image"
+                  />
                 </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="1">
-            <div className="media media-50">
-              <img
-                className="rounded"
-                src="/assets/images/message/pic2.jpg"
-                alt="image"
-              />
-            </div>
-            <div className="media-content">
-              <div>
-                <h6 className="name">Emilia Green</h6>
-                <p className="my-1">
-                  Can you share your current location now sis
-                </p>
-              </div>
-              <div className="left-content">
-                <span className="time">2m ago</span>
-                <div className="seen-btn mt-2">
-                  <svg
-                    width="11"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
-                      fill="#BBB6D0"
-                    />
-                  </svg>
+                <div className="media-content">
+                  <div>
+                    <h6 className="name">
+                      {secondUser?.first_name + " " + secondUser?.last_name}
+                    </h6>
+                    <p className="my-1">{li.messages?.[0]?.content}</p>
+                  </div>
+                  <div className="left-content" style={{ width: "auto" }}>
+                    <span className="time">
+                      {timeAgo(li.messages?.[0]?.createdAt)}
+                    </span>
+                    <div
+                      className={`seen-btn mt-2 ${
+                        li.messages?.[0]?.seen ? "active" : ""
+                      }`}
+                    >
+                      <svg
+                        width="11"
+                        height="9"
+                        viewBox="0 0 11 9"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
+                          fill="#BBB6D0"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="1">
-            <div className="media media-50">
-              <img
-                className="rounded"
-                src="/assets/images/message/pic3.jpg"
-                alt="image"
-              />
-            </div>
-            <div className="media-content">
-              <div>
-                <h6 className="name">Richard lee</h6>
-                <p className="my-1">Cmon dude! make it fast, let’s go</p>
-              </div>
-              <div className="left-content">
-                <span className="time">2m ago</span>
-                <div className="seen-btn mt-2">
-                  <svg
-                    width="11"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
-                      fill="#BBB6D0"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="1">
-            <div className="media media-50">
-              <img
-                className="rounded"
-                src="/assets/images/message/pic4.jpg"
-                alt="image"
-              />
-            </div>
-            <div className="media-content">
-              <div>
-                <h6 className="name">Hendri Lee</h6>
-                <p className="my-1">Did you tell him about your new car?</p>
-              </div>
-              <div className="left-content">
-                <span className="time">2m ago</span>
-                <div className="seen-btn active mt-2">
-                  <svg
-                    width="11"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
-                      fill="#BBB6D0"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="1">
-            <div className="media media-50">
-              <img
-                className="rounded"
-                src="/assets/images/message/pic5.jpg"
-                alt="image"
-              />
-            </div>
-            <div className="media-content">
-              <div>
-                <h6 className="name">Mogan Fox</h6>
-                <p className="my-1">Let's go!</p>
-              </div>
-              <div className="left-content">
-                <span className="time">2m ago</span>
-                <div className="seen-btn mt-2">
-                  <svg
-                    width="11"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M3.73609 8.82324L0.161085 4.90081C-0.0536949 4.66516 -0.0536949 4.28308 0.161085 4.0474L0.938884 3.19399C1.15366 2.95832 1.50193 2.95832 1.71671 3.19399L4.125 5.8363L9.28329 0.176739C9.49807 -0.058913 9.84634 -0.058913 10.0611 0.176739L10.8389 1.03015C11.0537 1.2658 11.0537 1.64789 10.8389 1.88356L4.51391 8.82326C4.29911 9.05892 3.95087 9.05892 3.73609 8.82324Z"
-                      fill="#BBB6D0"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </Link>
+              </Link>
+            );
+          })}
         </li>
       </ul>
     </>
