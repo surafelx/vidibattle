5beaf6d57e89973063ab9c78a3084fe6039a0de3
abraham <aaabrham421@gmail.com>
2023-10-28 00:58:51 +0300

    feat: add socket connection only for logged-in user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,19 @@ import { useEffect } from "react";
 import "./App.css";
 import Router from "./Routes";
 import socket, { initializeSocket } from "./services/socket";
+import { getUser, getUserId } from "./services/auth";
 
 function App() {
   useEffect(() => {
-    // TODO: create socket for authenticated user
-    // create socket connection
-    initializeSocket();
+    if (getUser() !== null && getUserId() !== null && !socket.connected) {
+      // create socket connection for logged in users
+      initializeSocket();
 
-    return () => {
-      // Clean up the socket connection if necessary
-      socket.disconnect();
-    };
+      return () => {
+        // Clean up the socket connection if necessary
+        socket.disconnect();
+      };
+    }
   }, []);
 
   return (
