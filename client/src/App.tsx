1921d12ed52c9b5eb039ef3945a6a33c3db83b3e
abraham <aaabrham421@gmail.com>
2023-10-27 18:42:32 +0300

    feat: socket.io integration on the fronend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import Router from "./Routes";
-import Login from "./pages/auth/Login";
+import socket, { initializeSocket } from "./services/socket";
 
 function App() {
+  useEffect(() => {
+    // TODO: create socket for authenticated user
+    // create socket connection
+    initializeSocket();
+
+    return () => {
+      // Clean up the socket connection if necessary
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <Router />
---
+++ client/src/services/socket.ts
@@ -0,0 +1,19 @@
+import { io } from "socket.io-client";
+import { env } from "../env";
+
+const socket = io(env.VITE_WEB_SOCKET_URL); // Connect to the server's WebSocket endpoint
+
+export function initializeSocket() {
+  // establish connection
+  socket.connect();
+
+  socket.on("connect", () => {
+    console.log("Socket connected");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected");
+  });
+}
+
+export default socket;
