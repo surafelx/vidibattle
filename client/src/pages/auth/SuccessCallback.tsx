d1dd2e3bf3493ce66ff32d5ee5109186646a7931
abraham <aaabrham421@gmail.com>
2023-10-28 00:56:18 +0300

    feat: add component to set localstorage after a successfull login

---
+++ client/src/pages/auth/SuccessCallback.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useCurrentUserStore } from "../../store";
+import { get } from "../../services/crud";
+import { setUser, setUserId } from "../../services/auth";
+import SplashScreen from "../../components/SplashScreen";
+
+export default function SuccessCallback() {
+  const setUserId_store = useCurrentUserStore((s: any) => s.setCurrentUserId);
+  const setCurrentUser_store = useCurrentUserStore(
+    (s: any) => s.setCurrentUserId
+  );
+
+  useEffect(() => {
+    get("user/authenticated")
+      .then((res) => {
+        setUserId_store(res.data._id);
+        setCurrentUser_store(res.data);
+        setUserId(res.data._id);
+        setUser(res.data);
+        window.location.replace("/home");
+      })
+      .catch(() => {
+        window.location.replace("/auth/login");
+      });
+  }, []);
+
+  return <SplashScreen />;
+}
