ebb7816286243c049b0af3d49d1bcda05ad93c06
abraham <aaabrham421@gmail.com>
2023-10-28 00:54:54 +0300

    feat: send credentials with every request, redirect unauthorized users to login

--- client/src/services/crud.ts
+++ client/src/services/crud.ts
@@ -3,46 +3,58 @@ import { env } from "../env";
 
 const BASE_URL = env.VITE_API_URL;
 
-export const create = async (url: string, data: any) => {
+export const get = async (url: string, params?: any) => {
   try {
-    const response = await axios.post(`${BASE_URL}/${url}`, data);
+    const response = await axios.get(
+      `${BASE_URL}/${url}`,
+      params ? { withCredentials: true, ...params } : { withCredentials: true }
+    );
     return response.data;
-  } catch (error) {
-    console.error("error:", error);
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
     throw error;
   }
 };
 
-export const get = async (url: string, params?: any) => {
-  // TODO: add query params if any
+export const create = async (url: string, data: any) => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/${url}`,
-      params ? { params } : {}
-    );
+    const response = await axios.post(`${BASE_URL}/${url}`, data, {
+      withCredentials: true,
+    });
     return response.data;
-  } catch (error) {
-    console.error("error:", error);
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
     throw error;
   }
 };
 
 export const update = async (url: string, data: any) => {
   try {
-    const response = await axios.put(`${BASE_URL}/${url}`, data);
+    const response = await axios.put(`${BASE_URL}/${url}`, data, {
+      withCredentials: true,
+    });
     return response.data;
-  } catch (error) {
-    console.error("error:", error);
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
     throw error;
   }
 };
 
 export const remove = async (url: string) => {
   try {
-    const response = await axios.delete(`${BASE_URL}/${url}/`);
+    const response = await axios.delete(`${BASE_URL}/${url}/`, {
+      withCredentials: true,
+    });
     return response.data;
-  } catch (error) {
-    console.error("error:", error);
+  } catch (error: any) {
+    handleStatusCodes(error.response.status);
     throw error;
   }
 };
+
+function handleStatusCodes(status: number) {
+  switch (status) {
+    case 401:
+      window.location.replace("/auth");
+  }
+}
