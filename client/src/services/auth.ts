8c1506a134b6ee4fcb8ec022f8c39615ac78acfe
abraham <aaabrham421@gmail.com>
2023-10-28 17:05:12 +0300

    feat: add localstorage getter and setter functions

---
+++ client/src/services/auth.ts
@@ -0,0 +1,19 @@
+export const setUserId = (id: string) => {
+  localStorage.setItem("userId", JSON.stringify(id));
+};
+
+export const getUserId = () => {
+  return localStorage.getItem("userId")
+    ? JSON.parse(localStorage.getItem("userId") ?? "")
+    : null;
+};
+
+export const setUser = (user: any) => {
+  localStorage.setItem("user", JSON.stringify(user));
+};
+
+export const getUser = () => {
+  return localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user") ?? "")
+    : null;
+};
