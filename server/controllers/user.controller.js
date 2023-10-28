a7fa74743abe4ef1436ba27cf382490b63783a4f
abraham <aaabrham421@gmail.com>
2023-10-28 22:10:41 +0300

    feat: add api endpoint for getting authenticated user info

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -12,3 +12,12 @@ module.exports.getBasicUserInfo = async (req, res, next) => {
     next(e);
   }
 };
+
+module.exports.getAuthenticatedUser = async (req, res, next) => {
+  try {
+    const { _id, first_name, last_name, profile_img } = req.user;
+    res.json({ data: { _id, first_name, last_name, profile_img } });
+  } catch (e) {
+    next(e);
+  }
+};
