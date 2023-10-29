a104c940a38e292b3de000f6872e8ed5ff6e2e2d
abraham <aaabrham421@gmail.com>
2023-10-29 22:35:49 +0300

    feat: add post preview componenet

---
+++ client/src/services/file.ts
@@ -0,0 +1,11 @@
+export function formatFileSize(sizeInBytes: number): string {
+  if (sizeInBytes < 1024) {
+    return sizeInBytes + " bytes";
+  } else if (sizeInBytes < 1024 * 1024) {
+    const sizeInKB = (sizeInBytes / 1024).toFixed(2);
+    return sizeInKB + " KB";
+  } else {
+    const sizeInMB = (sizeInBytes / (1024 * 1024)).toFixed(2);
+    return sizeInMB + " MB";
+  }
+}
