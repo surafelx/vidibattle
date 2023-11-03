ca8f7ff2ae41b9b7b47f612d268b69452dcb400e
abraham <aaabrham421@gmail.com>
2023-11-04 00:04:47 +0300

    feat: add react-toastify library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Router from "./Routes";
 import socket, { initializeSocket } from "./services/socket";
 import { getUser, getUserId } from "./services/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   useEffect(() => {
@@ -20,6 +22,7 @@ function App() {
   return (
     <>
       <Router />
+      <ToastContainer />
     </>
   );
 }
