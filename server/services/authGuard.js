627f47c89ae13bb6b8ba4ef941124836af3aa7a8
abraham <aaabrham421@gmail.com>
2023-11-04 15:30:39 +0300

    feat: add admin auth guard

--- server/services/authGuard.js
+++ server/services/authGuard.js
@@ -4,3 +4,9 @@ module.exports.authGuard = (req, res, next) => {
 
   res.status(401).json({ message: "unauthorized" });
 };
+
+module.exports.adminAuthGuard = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.is_admin === true) return next();
+
+  res.status(401).json({ message: "unauthorized" });
+};
