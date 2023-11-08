6134bbd03a5bd3f6aa3019ca10db1f760611097a
abraham <aaabrham421@gmail.com>
2023-11-08 16:32:16 +0300

    fix: modifications to auth guard

--- server/services/authGuard.js
+++ server/services/authGuard.js
@@ -1,12 +1,12 @@
 // Middleware to check if user is authenticated
 module.exports.authGuard = (req, res, next) => {
-  if (req.isAuthenticated()) return next();
+  if (req.isAuthenticated() && !req.user.is_admin) return next();
 
-  res.status(401).json({ message: "unauthorized" });
+  return res.status(401).json({ message: "unauthorized" });
 };
 
 module.exports.adminAuthGuard = (req, res, next) => {
   if (req.isAuthenticated() && req.user.is_admin === true) return next();
 
-  res.status(401).json({ message: "unauthorized" });
+  return res.status(401).json({ message: "unauthorized" });
 };
