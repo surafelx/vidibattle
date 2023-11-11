a4fe94e1652662a946d8cdfe6abdf0170b191dfa
abraham <aaabrham421@gmail.com>
2023-11-11 20:44:38 +0300

    fix: fix #5 for express-session prod error

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ const connect = mongoose
           maxAge: 1000 * 60 * 60 * 24, // 1 day,
           secure: "auto",
           sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
-          domain: process.env.CLIENT_URL,
+          domain: process.env.API_URL,
           path: "/"
         },
       })
