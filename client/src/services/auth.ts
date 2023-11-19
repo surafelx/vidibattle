8f2bc847f589e7f2a5152266b07ae717178c091a
abraham <aaabrham421@gmail.com>
2023-11-19 19:53:31 +0300

    feat: add session storage

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,27 +1,39 @@
 export const setUserId = (id: string) => {
+  sessionStorage.setItem("userId", JSON.stringify(id));
   localStorage.setItem("userId", JSON.stringify(id));
 };
 
 export const getUserId = () => {
-  return localStorage.getItem("userId")
-    ? JSON.parse(localStorage.getItem("userId") ?? "")
-    : null;
+  if (sessionStorage.getItem("userId")) {
+    return JSON.parse(sessionStorage.getItem("userId") ?? "");
+  }
+
+  if (localStorage.getItem("userId")) {
+    return JSON.parse(localStorage.getItem("userId") ?? "");
+  }
+
+  return null;
 };
 
 export const getUsername = () => {
-  return localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user") ?? "")?.username
-    : null;
+  return getUser()?.username;
 };
 
 export const setUser = (user: any) => {
+  sessionStorage.setItem("user", JSON.stringify(user));
   localStorage.setItem("user", JSON.stringify(user));
 };
 
 export const getUser = () => {
-  return localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user") ?? "")
-    : null;
+  if (sessionStorage.getItem("user")) {
+    return JSON.parse(sessionStorage.getItem("user") ?? "");
+  }
+
+  if (localStorage.getItem("user")) {
+    return JSON.parse(localStorage.getItem("user") ?? "");
+  }
+
+  return null;
 };
 
 export const isLoggedIn = () => {
@@ -42,6 +54,8 @@ export const updateUserData = (user: any) => {
 };
 
 export const clearAuth = () => {
+  sessionStorage.removeItem("user");
+  sessionStorage.removeItem("userId");
   localStorage.removeItem("user");
   localStorage.removeItem("userId");
 };
