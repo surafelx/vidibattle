cf1e995235a9c6acb82e3573a2295fa074ed3f7c
abraham <aaabrham421@gmail.com>
2023-11-22 21:29:49 +0300

    feat: add default competition image handling

--- client/src/services/asset-paths.ts
+++ client/src/services/asset-paths.ts
@@ -3,6 +3,7 @@ import { env } from "../env";
 export const defaultThumbnail = "/assets/images/default-thumbnail.png";
 export const defaultPost = "/assets/images/default-post.png";
 export const defaultUser = "/assets/images/default-user.png";
+export const defaultCompetition = "/assets/images/default-post.png";
 
 export const handleProfileImageError = (event: any) => {
   event.target.src = defaultUser;
@@ -10,6 +11,10 @@ export const handleProfileImageError = (event: any) => {
 
 export const handlePostImageError = (event: any) => {
   event.target.src = defaultPost;
+};
+
+export const handleCompetitionImageError = (event: any) => {
+  event.target.src = defaultCompetition;
 };
 
 export const formatResourceURL = (filename: string) => {
