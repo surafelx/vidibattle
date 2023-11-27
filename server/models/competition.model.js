f2b58e14fd25ce9ab0f7bdb4d0fc91d251d9861b
abraham <aaabrham421@gmail.com>
2023-11-27 11:42:33 +0300

    feat: modify competition schema, add competing users and rounds schemas and models

--- server/models/competition.model.js
+++ server/models/competition.model.js
@@ -1,4 +1,13 @@
 const { default: mongoose } = require("mongoose");
-const { competitionSchema } = require("../schemas/competition.schema");
+const {
+  competitionSchema,
+  roundSchema,
+  competingUserSchema,
+} = require("../schemas/competition.schema");
 
 module.exports.Competition = mongoose.model("Competition", competitionSchema);
+module.exports.Round = mongoose.model("Round", roundSchema);
+module.exports.CompetingUser = mongoose.model(
+  "CompetingUser",
+  competingUserSchema
+);
--- server/schemas/competition.schema.js
+++ server/schemas/competition.schema.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
@@ -6,20 +7,60 @@ const competitionSchema = Schema(
   {
     name: { type: String, required: true },
     description: { type: String },
-    start_date: { type: Date, required: true },
-    end_date: { type: Date, required: true },
+
     status: {
       type: String,
-      enum: ["scheduled", "started", "ended"],
+      enum: ["scheduled", "started", "ended", "cancelled"],
       default: "scheduled",
     },
     type: { type: String, enum: ["video", "image", "any"], default: "any" },
-    winning_posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    winners: [{ type: Schema.Types.ObjectId, ref: "User" }],
     is_paid: { type: Boolean, default: false },
     amount: { type: Number, default: 0 },
     image: { type: String },
+    current_round: { type: Number, required: true, default: 1 },
+    rounds_count: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+const roundSchema = Schema(
+  {
+    number: { type: Number, required: true },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, required: true },
+    min_likes: { type: Number, required: true },
+    is_last_round: { type: Boolean, default: false },
+    competition: { type: Schema.Types.ObjectId, ref: "Competition" },
+  },
+  { timestamps: true }
+);
+
+roundSchema.plugin(uniqueValidator);
+
+const competingUserSchema = Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    competition: {
+      type: Schema.Types.ObjectId,
+      ref: "Competition",
+      required: true,
+    },
+    current_round: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    status: {
+      type: String,
+      enum: ["playing", "won", "lost", "removed", "left"],
+      default: "playing",
+    },
+    removed_reason: { type: String },
   },
   { timestamps: true }
 );
 
 module.exports.competitionSchema = competitionSchema;
+module.exports.roundSchema = roundSchema;
+module.exports.competingUserSchema = competingUserSchema;
