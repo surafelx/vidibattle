91e26469636838aece35a0aa3e0e0e01d4235c78
abraham <aaabrham421@gmail.com>
2023-11-28 12:27:01 +0300

    fix: fix on cancel competition api

--- server/controllers/competition.controller.js
+++ server/controllers/competition.controller.js
@@ -340,7 +340,7 @@ module.exports.cancelCompetition = async (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const competition = await Competition.findOne({ id });
+    const competition = await Competition.findOne({ _id: id });
 
     if (
       !competition ||
