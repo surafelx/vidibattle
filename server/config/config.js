3c2954fa3cdbcef9d0fd409417a534e5c2ddf16e
abraham <aaabrham421@gmail.com>
2023-12-05 20:28:15 +0300

    fix: paytm config modification

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,7 @@ var PaytmConfig = {
   mid: process.env.MERCHANT_ID,
   key: process.env.MERCHANT_KEY,
   website: process.env.MERCHANT_WEBSITE,
+  callbackURL: process.env.API_URL + "/api/wallet/load/callback",
 };
+
 module.exports.PaytmConfig = PaytmConfig;
