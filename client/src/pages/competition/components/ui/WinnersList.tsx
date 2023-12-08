27546e7719c7519568b10e00d1d35176c705859d
abraham <aaabrham421@gmail.com>
2023-12-08 18:04:06 +0300

    feat: add winners list

--- client/src/pages/competition/components/ui/WinnersList.tsx
+++ client/src/pages/competition/components/ui/WinnersList.tsx
@@ -1,5 +1,72 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  formatResourceURL,
+  handleProfileImageError,
+} from "../../../../services/asset-paths";
+import { getName } from "../../../../services/utils";
 
 export default function WinnersList({ winners }: { winners: any[] }) {
-  return <div>WinnersList</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="text-success" style={{ textDecoration: "underline" }}>
+        Winners
+      </h1>
+
+      <div className="d-flex flex-column gap-2 mt-2">
+        {winners.map((winner) => (
+          <>
+            <div className="d-flex gap-3 justify-content-center align-items-center">
+              <div
+                style={{
+                  borderRadius: "20px",
+                  marginBottom: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: "2px",
+                  background:
+                    "linear-gradient(180deg, #FE9063 0%, #704FFE 100%)",
+                  position: "relative",
+                  cursor: "pointer",
+                }}
+                onClick={() => navigate("/profile/" + winner?.user?.username)}
+              >
+                <img
+                  src={formatResourceURL(winner?.user?.profile_img)}
+                  onError={handleProfileImageError}
+                  style={{
+                    width: "100px",
+                    minWidth: "100px",
+                    height: "100px",
+                    boxSizing: "content-box",
+                    borderRadius: "18px",
+                    border: "6px solid #FEF3ED",
+                  }}
+                />
+              </div>
+              <div className="">
+                <div
+                  onClick={() => navigate("/profile/" + winner?.user?.username)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <div className="text-secondary fw-bold pb-0 lh-sm">
+                    {getName(winner?.user)}
+                  </div>
+                  <div className="small text-info pb-1">
+                    @{winner?.user?.username}
+                  </div>
+                </div>
+                <div className="small text-dark">
+                  Likes:&nbsp;{winner?.likes}
+                </div>
+                <div className="small text-dark">Rank:&nbsp;{winner?.rank}</div>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
