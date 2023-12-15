c4a93a3a5bb54ad206b44f01415df533e4280c41
abraham <aaabrham421@gmail.com>
2023-12-15 18:44:48 +0300

    feat: add uniqueness to configuration key

--- server/schemas/configuration.schema.js
+++ server/schemas/configuration.schema.js
@@ -1,14 +1,22 @@
 const { default: mongoose } = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const configurationSchema = new Schema(
   {
-    key: { type: String, required: true },
+    key: {
+      type: String,
+      required: true,
+      unique: true,
+      uniqueCaseInsensitive: true,
+    },
     value: { type: Schema.Types.Mixed, required: true },
     metadata: { type: Schema.Types.Mixed, default: {} },
   },
   { timestamps: true }
 );
+
+configurationSchema.plugin(uniqueValidator);
 
 module.exports.configurationSchema = configurationSchema;
