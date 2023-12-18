5282c62ad4d412962906893cd5151fe54385632a
abraham <aaabrham421@gmail.com>
2023-12-18 11:05:37 +0300

    refactor: rename post controller

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const postsController = require("../controllers/posts.controller");
+const postsController = require("../controllers/post.controller");
 const { upload } = require("../services/storage");
 const { authGuard, adminAuthGuard } = require("../services/authGuard");
 
