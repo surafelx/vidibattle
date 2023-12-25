c7a332b08fb396583b9927b79f514ebbd1f6199c
abraham <aaabrham421@gmail.com>
2023-12-25 23:35:53 +0300

    fix: date to utc function fix

--- server/services/date.js
+++ server/services/date.js
@@ -1,16 +1,14 @@
-module.exports.dateToUTC = (date) =>  {
+module.exports.dateToUTC = (date) => {
   if (!date) {
     return false;
   }
 
-  if (typeof date === "string") {
-    date = new Date(date);
-  }
+  date = new Date(date);
   const utcDate = date.getTime();
   return utcDate;
-}
+};
 
-module.exports.UTCtoDate = (utcDate) =>  {
+module.exports.UTCtoDate = (utcDate) => {
   const date = new Date(utcDate);
   return date;
-}
+};
