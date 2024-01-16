7f64c9182be3d38fd4245990554a23977aa56ec1
abraham <aaabrham421@gmail.com>
2024-01-16 17:25:58 +0300

    feat: add wallet link to settings page

--- client/src/pages/setting/Setting.tsx
+++ client/src/pages/setting/Setting.tsx
@@ -63,6 +63,29 @@ export default function Setting() {
               </li>
               <li>
                 <a
+                  onClick={() => navigate("/wallet")}
+                  style={{ cursor: "pointer" }}
+                  className="item-content item-link filter m-0"
+                >
+                  <div className="dz-icon icon-sm">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-wallet"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a2 2 0 0 1-1-.268M1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1" />
+                    </svg>
+                  </div>
+                  <div className="dz-inner">
+                    <h6 className="mb-0">Wallet</h6>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a
                   onClick={() => navigate("/privacy-policy")}
                   style={{ cursor: "pointer" }}
                   className="item-content item-link filter m-0"
