277bd2f965233cf77545e05cbd29b8f6960fa241
abraham <aaabrham421@gmail.com>
2024-01-16 17:26:49 +0300

    delete paytm code

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-var PaytmConfig = {
-  mid: process.env.MERCHANT_ID,
-  key: process.env.MERCHANT_KEY,
-  website: process.env.MERCHANT_WEBSITE,
-  callbackURL: process.env.API_URL + "/api/wallet/load/callback",
-};
-
-module.exports.PaytmConfig = PaytmConfig;
