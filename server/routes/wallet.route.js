4255745faf5cabf9e47fa9ff537f3036077979db
abraham <aaabrham421@gmail.com>
2024-01-16 17:42:09 +0300

    feat: add create order and verify order apis

--- server/routes/wallet.route.js
+++ server/routes/wallet.route.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const {
   getWalletInfo,
-  loadToWallet,
-  loadToWalletCallback,
-  loadToWallet1,
+  createOrder,
+  verifyPayment,
 } = require("../controllers/wallet.controller");
 const router = express.Router();
 
 // get
 router.get("/:userId/info", getWalletInfo);
-router.get("/load/test", loadToWallet1); // TODO: delete later
 
 // post
-router.post("/load", loadToWallet);
-router.post("/load/callback", loadToWalletCallback);
+router.post("/create-order", createOrder);
+router.post("/verify", verifyPayment);
 
 module.exports = router;
