04120c99a30f11b539a8c1eaf29bc2a2121586fb
abraham <aaabrham421@gmail.com>
2024-01-18 18:26:04 +0300

    style: change blinking circles color

--- client/src/components/BlinkingLoadingCircles.tsx
+++ client/src/components/BlinkingLoadingCircles.tsx
@@ -3,23 +3,23 @@ export default function BlinkingLoadingCircles() {
     <div className="card-body d-flex justify-content-center">
       <div className="dz-spinner d-flex align-items-center">
         <div
-          className="spinner-grow spinner-grow-sm me-2 mb-2 text-success"
+          className="spinner-grow spinner-grow-sm me-2 mb-2"
           role="status"
-          style={{animationDelay: "100ms"}}
+          style={{ animationDelay: "100ms", color: "#FF671F" }}
         >
           <span className="sr-only">Loading...</span>
         </div>
         <div
-          className="spinner-grow spinner-grow-sm me-2 mb-2 text-warning"
+          className="spinner-grow spinner-grow-sm me-2 mb-2"
           role="status"
-          style={{animationDelay: "200ms"}}
+          style={{ animationDelay: "200ms", color: "#FFFFFF" }}
         >
           <span className="sr-only">Loading...</span>
         </div>
         <div
-          className="spinner-grow spinner-grow-sm me-2 mb-2 text-danger"
+          className="spinner-grow spinner-grow-sm me-2 mb-2"
           role="status"
-          style={{animationDelay: "300ms"}}
+          style={{ animationDelay: "300ms", color: "#046A38" }}
         >
           <span className="sr-only">Loading...</span>
         </div>
