bb0867e5f4b6fd145a2951581aee0cb0ca13c258
abraham <aaabrham421@gmail.com>
2024-01-22 17:22:05 +0300

    fix: fix typo

--- client/src/components/HeaderLogo.tsx
+++ client/src/components/HeaderLogo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function HeaderLogo() {
   return (
     <Link to="/home">
-      <img src="/assets/images/favicon.png" style={{ width: "40px" }} />;
+      <img src="/assets/images/favicon.png" style={{ width: "40px" }} />
     </Link>
   );
 }
