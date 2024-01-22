e06dd0ef20ce1f6f3e403908881bf5a37c2f321f
abraham <aaabrham421@gmail.com>
2024-01-22 16:37:32 +0300

    fix: make header logo clickable

--- client/src/components/HeaderLogo.tsx
+++ client/src/components/HeaderLogo.tsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function HeaderLogo() {
-  return <img src="/assets/images/favicon.png" style={{ width: "40px" }} />;
+  return (
+    <Link to="/home">
+      <img src="/assets/images/favicon.png" style={{ width: "40px" }} />;
+    </Link>
+  );
 }
