8a721046a613655c9ec0fb80c601535ea61f54d8
abraham <aaabrham421@gmail.com>
2024-01-23 18:44:37 +0300

    style: style fix on winners list

--- client/src/pages/competition/components/ui/WinnersList.tsx
+++ client/src/pages/competition/components/ui/WinnersList.tsx
@@ -10,59 +10,62 @@ export default function WinnersList({ winners }: { winners: any[] }) {
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-      <h1 className="text-success" style={{ textDecoration: "underline" }}>
+      <h1 className="text-light" style={{ textDecoration: "underline" }}>
         Winners
       </h1>
 
-      <div className="d-flex flex-column gap-2 mt-2">
-        {winners.map((winner) => (
-          <div
-            key={winner._id}
-            className="d-flex gap-3 justify-content-center align-items-center"
-          >
+      <div className="d-flex flex-column mt-2">
+        <div className="d-flex flex-column align-items-start gap-2">
+          {winners.map((winner) => (
             <div
-              style={{
-                borderRadius: "20px",
-                marginBottom: "5px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                padding: "2px",
-                background: "linear-gradient(180deg, #FE9063 0%, #704FFE 100%)",
-                position: "relative",
-                cursor: "pointer",
-              }}
-              onClick={() => navigate("/profile/" + winner?.user?.username)}
+              key={winner._id}
+              className="d-flex gap-3 justify-content-center align-items-center"
             >
-              <img
-                src={formatResourceURL(winner?.user?.profile_img)}
-                onError={handleProfileImageError}
-                style={{
-                  width: "100px",
-                  minWidth: "100px",
-                  height: "100px",
-                  boxSizing: "content-box",
-                  borderRadius: "18px",
-                  border: "6px solid #FEF3ED",
-                }}
-              />
-            </div>
-            <div className="">
               <div
+                style={{
+                  borderRadius: "20px",
+                  marginBottom: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: "2px",
+                  background:
+                    "linear-gradient(180deg, #FE9063 0%, #704FFE 100%)",
+                  position: "relative",
+                  cursor: "pointer",
+                }}
                 onClick={() => navigate("/profile/" + winner?.user?.username)}
-                style={{ cursor: "pointer" }}
               >
-                <div className="text-secondary fw-bold pb-0 lh-sm">
-                  {getName(winner?.user)}
-                </div>
-                <div className="small text-info pb-1">
-                  @{winner?.user?.username}
-                </div>
+                <img
+                  src={formatResourceURL(winner?.user?.profile_img)}
+                  onError={handleProfileImageError}
+                  style={{
+                    width: "100px",
+                    minWidth: "100px",
+                    height: "100px",
+                    boxSizing: "content-box",
+                    borderRadius: "18px",
+                    border: "6px solid #FEF3ED",
+                  }}
+                />
               </div>
-              <div className="small text-dark">Rank:&nbsp;{winner?.rank}</div>
+              <div className="">
+                <div
+                  onClick={() => navigate("/profile/" + winner?.user?.username)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <div className="text-secondary fw-bold pb-0 lh-sm">
+                    {getName(winner?.user)}
+                  </div>
+                  <div className="small text-info pb-1">
+                    @{winner?.user?.username}
+                  </div>
+                </div>
+                <div className="small text-dark">Rank:&nbsp;{winner?.rank}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
