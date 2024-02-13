ae3abd72a4ccfdf9c8d14deab84db9ad1a23ac76
aaabrham <aaabrham421@gmail.com>
2024-02-13 12:53:56 +0300

    fix: extend cookie life to 3 days

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ const connect = mongoose
         store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI ?? "" }),
         saveUninitialized: true,
         cookie: {
-          maxAge: 1000 * 60 * 60 * 24, // 1 day,
+          maxAge: 1000 * 60 * 60 * 3, // 3 days,
           secure: process.env.NODE_ENV === "development" ? false : true,
           sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
         },
